f9445bfcd2c95c2f528dc79c2141e47020128c1c
Josue G <josue.ggh@gmail.com>
2014-09-28 23:37:24 -0500

    Adding example

---
+++ examples/js/app.js
@@ -0,0 +1,92 @@
+monkeyApp = angular.module('monkeyApp',['ngMonkeylearn'] );
+
+/*
+ * You must configure the Monkey Learn Provider before start using in your Application
+ */
+monkeyApp.config([ '$monkeyProvider', function( $monkeyProvider) {
+
+  //Set your token
+  $monkeyProvider.setToken( 'd9046ad91abf604e8f9225290723fa085029f155' );
+
+  /* 
+   * Set the available methods in your account
+   * The structure of the object is : { 'ALIAS' : 'KEY '}
+   */
+  $monkeyProvider.setMethods( 
+    {
+      'retail_classify'   : 'YX4sJbxb' ,
+      'lang_detection_v2' : 'oJNMkt2V' ,
+      'topic_classifier'  : '5icAVzKR' ,
+      'lang_detection_v1' : 'hDDngsX8' ,
+    }
+  );
+
+}]);
+
+/*
+ * If you want to check if the token was correctly setting
+ * you can do it with 'getToken' method.
+ */
+monkeyApp.run(['$monkey' , function( $monkey){
+  console.log('Your token is : ' , $monkey.getToken() )
+}]);
+
+
+/* 
+ * Controller for first example
+ */
+
+monkeyApp.controller('monkeyCtrl', ['$scope', '$monkey',
+    function($scope,$monkey){
+      
+      $scope.text =  'Hello my friend. Are you looking for Monkey Learn?' ;
+
+      $monkey.classifyText('lang_detection_v1' , $scope.text).then(
+        function(data){
+          $scope.response = data;
+        }
+      );
+
+    }
+]);
+
+/* 
+ * Controller for second example
+ */
+ 
+monkeyApp.controller('monkeyAdvancedCtrl', ['$scope', '$monkey' , '$http' ,
+    function($scope,$monkey , $http){
+      
+      $scope.samples = [];
+      $http.get('data/text.json').success( function(data){
+          $scope.samples = data;
+        }
+      );
+
+      $scope.setText = function(sample){
+        $scope.text = sample;
+      }
+
+      $scope.classify = function( type ){
+        
+        $scope.response = null;
+        $scope.error = null;
+
+        if( $scope.text === undefined){
+          $scope.error = 'Please write a text to classify';
+          return false;
+        }
+
+        $monkey.classifyText( type  , $scope.text ).then(
+          function(data){
+            $scope.response = data;
+          }
+        );
+
+      }
+
+
+      
+
+    }
+]);
